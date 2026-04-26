ad89defc8d01df58e5f2f23e75ae62995cd8441f
AR-Infotech-DEV <arinfotechtdl@gmail.com>
2026-04-26 15:42:30 +0530

    aniket : branch tests

--- src/config/env.js
+++ src/config/env.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const env = {
+  
   nodeEnv: process.env.NODE_ENV || "development",
   port: Number(process.env.PORT || 3000),
   appName: process.env.APP_NAME || "flowupS",
